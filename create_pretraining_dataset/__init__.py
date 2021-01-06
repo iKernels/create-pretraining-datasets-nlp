276970e4cbf1eac733b3b1f2d8273b282a3f5047
lucadiliello <luca.diliello@unitn.it>
2021-01-06 02:59:54 +0100

    fixed typos

--- create_pretraining_dataset/__init__.py
+++
@@ -1,5 +0,0 @@
-from create_pretraining_dataset.utils import (CompressedDictionary,
-                                              create_examples,
-                                              documents_to_sentences,
-                                              multiprocessing_tokenizer,
-                                              prepare_datasets)
